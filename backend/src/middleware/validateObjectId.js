4b4c6a785b508940edd7dc502ff59ecc1a2faaaf
mikmak29 <mjogamaapple@gmail.com>
2025-12-19 07:59:12 +0800

    objectIdValidation added

---
+++ backend/src/middleware/validateObjectId.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const validateObjectId = (req, res, next) => {
+	const { id } = req.params;
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(400).json({
+			error: "Invalid ID format",
+			message: `"${id}" is not a valid MongoDB ObjectId`,
+		});
+	}
+
+	next();
+};
